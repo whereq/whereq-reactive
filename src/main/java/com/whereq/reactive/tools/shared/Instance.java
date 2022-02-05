1853b8323816b2dd8f36432563b52d3274261686
whereq <whereq@gmail.com>
2022-02-04 23:28:12 -0500

    Update Instance.java

--- src/main/java/com/whereq/reactive/tools/shared/Instance.java
+++ src/main/java/com/whereq/reactive/tools/shared/Instance.java
@@ -1,36 +1,18 @@
 package com.whereq.reactive.tools.shared;
 
-import org.h2.tools.Server;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
 
-import lombok.extern.slf4j.Slf4j;
+@Data
+public class Instance {
 
-@Component
-@Slf4j
-@ConditionalOnProperty(name = "whereq.reactive.h2-console-port")
-public class H2Console {
-    private Server server;
+    private int instanceNumber;
 
-    @Value("${whereq.reactive.h2-console-port}")
-    private Integer port;
+    private String instanceId;
 
-    @EventListener(ContextRefreshedEvent.class)
-    public void start() throws java.sql.SQLException {
-    	log.info("starting h2 console at port {}", port);
-        this.server = Server.createWebServer("-webPort", port.toString(), "-tcpAllowOthers").start();
-    }
+    private String hostName;
 
-    @EventListener(ContextClosedEvent.class)
-    public void stop() {
-        if(this.server != null) {
-            log.info("stopping h2 console at port {}", port);
-            this.server.stop();
-        }
-    }
+    private String status;
 
+    private String instanceJson;
 }
